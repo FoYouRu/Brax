8d67250efe0f81d11427129c3bc96fb3c97c654f
FoYouRu <tlaahrdud@naver.com>
2025-12-29 15:47:02 +0900

    train.py

--- brax/training/agents/clip_ed_sac/train.py
+++ brax/training/agents/clip_ed_sac/train.py
@@ -342,8 +342,8 @@ def train(
         
         # eigs = jnp.linalg.eigvals(A_batch)
         
-        # lambda_max = jnp.max(jnp.real(eigs))
-        lambda_max = jnp.max(jnp.abs(jnp.real(eigs)))
+        lambda_max = jnp.max(jnp.real(eigs))
+        # lambda_max = jnp.max(jnp.abs(jnp.real(eigs)))
         
         actor_loss, policy_params, policy_optimizer_state = actor_update(
             training_state.policy_params,
